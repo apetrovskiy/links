1802cecc03b44ef26a4aa6ab5fcd718b048295da
Alexander Petrovskiy <uiautomation@hotmail.com>
2013-06-19 03:16:39 +0400

    .

---
+++ SharePointScriptCenter.ps1
@@ -0,0 +1,5 @@
+http://gallery.technet.microsoft.com/scriptcenter/site/search?f%5B0%5D.Type=RootCategory&f%5B0%5D.Value=sharepoint&f%5B0%5D.Text=SharePoint
+http://gallery.technet.microsoft.com/scriptcenter/Create-SharePoint-Web-742a8fb9
+http://gallery.technet.microsoft.com/scriptcenter/Copy-all-SharePoint-Files-0999c53f
+http://gallery.technet.microsoft.com/scriptcenter/SPListEnumerator-PowerShell-b0ce0b9f
+http://gallery.technet.microsoft.com/scriptcenter/Retrieve-SharePoint-list-8e7fb421
